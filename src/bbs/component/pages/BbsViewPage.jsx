eed3877309ca2cf3924af9fac279fb105d6c117e
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-09-03 14:30:04 +0900

    add spring_ delete

--- src/bbs/component/pages/BbsViewPage.jsx
+++ src/bbs/component/pages/BbsViewPage.jsx
@@ -59,15 +59,38 @@ function BbsViewPage(props) {
         getBbs();
         // getComments();
     }, []);
+
+    // json server version
+    // const getComments = async () =>{
+    //     try{
+    //         const response = await api.get(`comments?bbsId=${id}`);
+    //         console.log(response.data.length);
+    //         setComments(response.data);
+    //     }catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+
+    // spring version
     const getComments = async () =>{
         try{
-            const response = await api.get(`comments?bbsId=${id}`);
-            console.log(response.data.length);
+            // user endpoint : bbs/comment/getComment
+            // (CommentRequestDTO params)
+            // const response = await api.get(`bbs/comment/getComment?bbsid=${id}`);
+
+            // user endpoint : bbs/comment/getComment/3
+            // (PathVariable(id="id") Integer id)
+            // map.put("bbsid", id)
+            const response = await api.get(`bbs/comment/getComment/${id}`);
+            console.log("debug >>> axios comments get response data, ",response.data);
+            console.log("debug >>> axios comments get response status, ",response.status);
+            console.log("debug >>> axios comments get response data length, ",response.data.length);
             setComments(response.data);
         }catch (error) {
             console.log(error);
         }
     }
+
     // json-server version
     // const getBbs = async() => {
     //     try{
@@ -84,7 +107,7 @@ function BbsViewPage(props) {
     const getBbs = async() => {
         try{
             const response = await api.get(`bbs/view/${id}`);
-            console.log("debug >>> axios get response data ,",response.data);
+            console.log("debug >>> axios Bbs get response data ,",response.data);
 
             setBbs(response.data);
             setComments(response.data.comments);
@@ -96,43 +119,101 @@ function BbsViewPage(props) {
     const textHandler = (e) => {
         setComment(e.target.value);
     }
+
+    // const commentHandler = async (bbsId, content) => {
+    //     console.log("debug >>>  comment btn click", bbsId, content);
+    //     if(content == ''){
+    //         alert('타임라인을 작성해 주세요!!')
+    //     } else{
+    //         const data = {
+    //             id : Date.now(),
+    //             content : content,
+    //             bbsId : bbsId
+    //         };
+    //         try{
+    //             const response = await api.post("comments/",data);
+    //             console.log("debug >>> axios get response data,", response.data);
+    //             alert("comment 등록 완료!!")
+    //             setComment('');
+    //             getComments();
+    //         }catch (err){
+    //             console.log(err);
+    //         }
+    //     }
+    // }
+
+    // spring version
     const commentHandler = async (bbsId, content) => {
         console.log("debug >>>  comment btn click", bbsId, content);
         if(content == ''){
             alert('타임라인을 작성해 주세요!!')
         } else{
             const data = {
-                id : Date.now(),
                 content : content,
-                bbsId : bbsId
+                bbsid : bbsId
             };
             try{
-                const response = await api.post("comments/",data);
-                console.log("debug >>> axios get response data,", response.data);
-                alert("comment 등록 완료!!")
-                setComment('');
-                getComments();
+                /*
+                1. commentRequestDTO 작성
+                2. Controller 에 bbs/comment/save 매핑메서드 정의하고
+                3 파라미터로 넘어오는 데이터 확인하기 까지만..
+                */
+                const response = await api.post("bbs/comment/save",data);
+                console.log("debug >>> axios get response ,", response);
+                console.log("debug >>> axios get response satatus,", response.status);
+                if(response.status == 204){
+                    alert("comment 등록 완료!!");
+                    setComment('');
+                    getComments();
+                } else {
+                    alert("타임라인 등록시 오류 발생");
+                }
             }catch (err){
                 console.log(err);
             }
         }
     }
-    const remveBbs = async (bbsId) => {
+    // json server version
+    // const remveBbs = async (bbsId) => {
+    //     console.log("remove btn click");
+    //     try{
+    //         const response = await api.delete(`bbs/${bbsId}`);
+    //         console.log("debug >>> axios get response data,", response.data);
+    //         alert("bbs 삭제 완료!!")
+    //         navigate("/");
+    //     }catch (err){
+    //         console.log(err);
+    //     }
+    // }
+    // spring version
+    /*
+    1. 조건처리를 통한 삭제여부 판단
+    2. comment가 있으면 삭제불가(alert)
+    3. 삭제가능할 경우 user endpoint = bbs/delete/${bbsid}
+    4. / 이동
+    */
+    const removeBbs = async (bbsId) => {
         console.log("remove btn click");
         try{
-            const response = await api.delete(`bbs/${bbsId}`);
-            console.log("debug >>> axios get response data,", response.data);
-            alert("bbs 삭제 완료!!")
-            navigate("/");
+            console.log("debug >>> comments legnth , ",comments.length);
+            if(comments.length == 0){
+                const response = await api.delete(`bbs/delete/${bbsId}`);
+                console.log("debug >>> axios get response status,", response.status);
+                navigate("/");
+            } else {
+                alert('게시글을 삭제할 수 없습니다.');
+            }
         }catch (err){
             console.log(err);
         }
     }
+
     const updateHandler = () =>{
         if(window.confirm("수정페이지로 이동하시겠습니까?")){
             navigate('/bbs-update',{state : {id : id}});
         }
     }
+    
     return (
         <Wrapper>
             <Container>
@@ -153,7 +234,7 @@ function BbsViewPage(props) {
                         onClick = {updateHandler}/>
                 &nbsp;&nbsp;&nbsp;
                 <Button title = "게시글 삭제하기"
-                        onClick = {()=>remveBbs(bbs.id)}/>
+                        onClick = {()=>removeBbs(bbs.id)}/>
             </PostContainer>
 
             <CommentLabel>타임라인</CommentLabel>
